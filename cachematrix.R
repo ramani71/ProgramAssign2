a7e4235ad9c3cfeb6175f4e097c51f439c9ffd82
Ramani Gopalan <ramani_71@hotmail.com>
2014-07-12 00:02:17 +0530

    draft
    draft

---
+++ cachematrix.R
@@ -0,0 +1,30 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## makeCacheMatrix function
+makeCacheMatrix <- function(x = numeric()) {
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) s <<- solve
+  getsolve <- function() s
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+}
+
+## cacheSolve function
+cacheSolve <- function(x, ...) {
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
+  s
+}
